8dc77bbcb0141893e93437d3ffd9d9008032bf13
hadley <h.wickham@gmail.com>
2008-10-10 15:51:13 -0500

    Parse and add csv result

---
+++ parse.r
@@ -0,0 +1,35 @@
+library(plyr)
+paths <- dir("data", pattern = "\\.txt$", full = T)
+
+
+header <- function(path) {
+  head <- readLines(path, 1) 
+  head <- gsub("TABLE: homesold,", "", head)
+  strsplit(head, ",")[[1]]
+}
+
+tidy_week <- function(path) {
+  df <- readLines(path)[-(1:2)]
+  head <- header(path)
+  
+  df <- gsub("[a-z]+: ", "", df)
+  records <- (seq_along(df) - 1) %/% (length(head) + 1)
+  # table(table(records)) 
+  out <- do.call("rbind", tapply(df, records, c))
+  colnames(out) <- c(head, "empty")
+  
+  out[, "price"] <- gsub("[$,]", "", out[, "price"])
+  out
+}
+
+one <- tidy_week(paths[1])
+
+all <- llply(paths, tidy_week, .progress = "text")
+all <- llply(all, as.data.frame, .progress = "text")
+df <- do.call("rbind.fill", all)
+save(df, file="all.rdata")
+
+df <- df[, c("county", "city", "zip", "street", "price", "br", "lsqft" ,"bsqft","year" ,"datesold")]
+df
+
+write.table(df, file = "house-sales.csv", quote=F, row=F, sep=",")
