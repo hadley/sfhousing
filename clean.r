a2999ec85d28da5ba506197f29abe25eeed43192
hadley <h.wickham@gmail.com>
2008-10-10 22:05:35 -0500

    More exploratory graphics

--- clean.r
+++ clean.r
@@ -6,23 +6,25 @@ h$year <- as.numeric(h$year)
 h$price <- as.numeric(h$price)
 
 h$datesold <- as.Date(h$datesold, format = "%m-%d-%y")
+h$street <- gsub("\\\\", "", h$street)
 
 qplot(price, data=h, geom="histogram", binwidth = 1e4, xlim=c(0, 2e6))
 qplot(price - round_any(price, 10000, floor), data=h, geom="histogram", binwidth=100)
 
 qplot(city, data=h, geom="bar")
 
-qplot(datesold, price, data=h, colour=city, geom="line", stat="summary", fun="median")
 
-source("geocode.r")
-
-
+# Focus on SF --------------------------------------------
+ad <- read.csv("addresses.csv")
+sf <- merge(sf, ad, by = c("street", "city", "zip"))
 
 sf <- subset(h, city %in% "San Francisco")
 qplot(datesold, data=sf, geom="bar", binwidth=1)
 qplot(datesold, data=sf, geom="bar", binwidth=7)
+qplot(datesold, log10(price), data=sf, geom="boxplot", group=round(datesold))
 
-h$street <- gsub("\\\\", "", h$street)
+
+qplot(long, lat, data = sf, size = I(0.1), ylim=c(37.7, 37.81), colour=price) + scale_colour_gradient(trans = "sqrt")
 
 # addresses <- sf[, c("street", "city", "zip")]
 # addresses$lat <- NA
