c49e68907af4d2b552d18b060fb9c6b6b4aed9a4
hadley <h.wickham@gmail.com>
2008-10-29 09:38:53 -0500

    Revised geocoding code

---
+++ geocode-google.r
@@ -0,0 +1,46 @@
+source("key.r")
+
+geocode_url <- function(address) {
+  geo <- "http://maps.google.com/maps/geo?"
+  
+  params <- c(
+    key = key,
+    q = address,
+    output = "csv"
+  )
+  
+  p <- paste(names(params), "=", laply(params,URLencode), sep="", collapse ="&")
+  paste(geo, p, sep="")
+}
+
+geocode <- function(addresses) {
+  paths <- laply(addresses, geocode_url)
+  
+  read <- function(path) {
+    suppressWarnings(read.csv(url(path), header = FALSE))
+  }
+  df <- ldply(paths, read, .progress = "text")
+  names(df) <- c("status", "accuracy", "lat", "long")
+  df
+}
+
+addy <- function(row) {
+  paste(row$street, ", ", row$city, " ", row$zip, sep="")
+}
+
+addresses <- read.csv("addresses.csv")
+
+while(TRUE) {
+  todo <- which(is.na(addresses$lat))[1:100]
+  todo <- todo[!is.na(todo)]
+  stopifnot(length(todo) > 0)
+  loc <- geocode(addy(addresses[todo, ]))
+
+  addresses[todo, names(loc)] <- loc
+  write.table(addresses, "addresses.csv", sep=",", row=F)
+}
+
+addresses[!is.na(addresses$accuracy) & addresses$accuracy == 0, c("lat", "long")] <- NA
+
+addresses[!is.na(addresses$lat) & addresses$lat < 30, c("lat", "long")] <- NA
+qplot(long, lat, data=addresses)
--- geocode.r
+++ geocode.r
@@ -1,46 +1,24 @@
-source("key.r")
+source("geocode-usc.r")
 
-geocode_url <- function(address) {
-  geo <- "http://maps.google.com/maps/geo?"
-  
-  params <- c(
-    key = key,
-    q = address,
-    output = "csv"
-  )
-  
-  p <- paste(names(params), "=", laply(params,URLencode), sep="", collapse ="&")
-  paste(geo, p, sep="")
+# Create empty addresses csv file if necessary.
+if (!file.exists("addresses.csv")) {
+  sales <- read.csv("house-sales.csv", stringsAsFactors = FALSE)
+  addresses <- unique(sales[, c("street", "city", "zip")])
+  addresses <- data.frame(addresses, "long" = NA, "lat" = NA, "quality" = NA_character_, "match" = NA_character_, "success" = NA_character_, "error" = NA_character_, stringsAsFactors = FALSE)
+  write.table(addresses, "addresses.csv", sep=",", row=F)  
 }
 
-geocode <- function(addresses) {
-  paths <- laply(addresses, geocode_url)
-  
-  read <- function(path) {
-    suppressWarnings(read.csv(url(path), header = FALSE))
-  }
-  df <- ldply(paths, read, .progress = "text")
-  names(df) <- c("status", "accuracy", "lat", "long")
-  df
-}
-
-addy <- function(row) {
-  paste(row$street, ", ", row$city, " ", row$zip, sep="")
-}
-
-addresses <- read.csv("addresses.csv")
-
+addresses <- read.csv("addresses.csv", stringsAsFactors = FALSE)
+# Loop through a chunk at a time, saving results to csv as we go.
 while(TRUE) {
-  todo <- which(is.na(addresses$lat))[1:100]
+  todo <- which(is.na(addresses$lat))[1:50]
   todo <- todo[!is.na(todo)]
   stopifnot(length(todo) > 0)
-  loc <- geocode(addy(addresses[todo, ]))
+  loc <- do.call(geocode, addresses[todo, 1:3])
+  stopifnot(ncol(loc) == 6)
+  stopifnot(nrow(loc) == 50)
 
   addresses[todo, names(loc)] <- loc
   write.table(addresses, "addresses.csv", sep=",", row=F)
 }
 
-addresses[!is.na(addresses$accuracy) & addresses$accuracy == 0, c("lat", "long")] <- NA
-
-addresses[!is.na(addresses$lat) & addresses$lat < 30, c("lat", "long")] <- NA
-qplot(long, lat, data=addresses)
