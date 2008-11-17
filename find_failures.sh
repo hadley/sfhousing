96992b4f8b823720dffef512484caeab51da84a4
Deborah Swayne <dfs@dt-dfs.research.att.com>
2008-11-17 17:06:06 -0500

    edited filters to clean addresses.csv and house-sales.csv; script goes on to identify failures

--- find_failures.sh
+++ find_failures.sh
@@ -1,34 +1,52 @@
-
-if ! test -f addresses.clean
-then
-  # make a cleaned up copy of addresses.csv
-  sed '1d' addresses.csv | sed 's/\"//g' |\
-    awk -F, 'NR > 1 && $2 != "" {print}' |\
-    sed 's/  / /; s/`b/B/; s/Discoverey/Discovery/; s/`g/G/; s/`h/H/; s/`los/Los/; s/`s/S/; s/tiburon/Tiburon/; s/`v/V/' > addresses.clean
-fi
+#!/bin/bash
 
 # The same filters will have to be applied to house-sales.csv
+if ! test -f addresses.clean
+then
+  # make a cleaned up copy of addresses.csv; remove quotation marks
+  # so it's easier to join this to house-sales (unix join, not R).
+  awk -F, 'BEGIN{OFS=","} {
+    gsub(/"/,"")
+    if ($2 != "") {
+     gsub(/  /," ")
+     sub(/`b/,"B",$2)
+     sub(/`g/,"G",$2)
+     sub(/`h/,"H",$2)
+     sub(/`l/,"L",$2)
+     sub(/`s/,"S",$2)
+     sub(/`v/,"V",$2)
+     sub(/tiburon/,"Tiburon",$2)
+     sub(/Discoverey/,"Discovery",$2)     
+     print
+   }}' addresses.csv > addresses-clean.csv
+
+  # apply the same filter to hous3-sales.csv
+  awk -F, 'BEGIN{OFS=","} {
+   gsub(/  /," ")
+   sub(/`b/,"B",$2)
+   sub(/`g/,"G",$2)
+   sub(/`h/,"H",$2)
+   sub(/`l/,"L",$2)
+   sub(/`s/,"S",$2)
+   sub(/`v/,"V",$2)
+   sub(/tiburon/,"Tiburon",$2)
+   sub(/Discoverey/,"Discovery",$2)
+   print
+  }' house-sales.csv > house-sales-clean.csv
+
+fi
+
 
 if ! test -f towns
 then
-  awk -F, '{print $2}' addresses.clean |\
+  awk -F, '{print $2}' addresses-clean.csv |\
      sort -u > towns
 fi
 
-rm -f failures.csv
-cat towns | while read line
-do
-  town=$line
-  echo $town
-  cat addresses.clean | sed 's/\"//g' |\
-    awk -F, -vtown="$town" 'BEGIN{OFS=","} {
-      if ($2 == town && ($6 ~ /CITY/ || $6 ~ /ZIP_CODE/ || $6 ~ /COUNTRY/ || $6 ~ /COUNTY/ || $6 ~ /UNMATCH/)) {
-        print $1, $3, $4, $5, $6
-      }}' |\
-      sort -k2 |\
-      awk -F, -vtown="$town" 'BEGIN{OFS=","} {{
-        print $1, town, $2, $3, $4, $5
-      }}' >>failures.csv
-done
+
+awk -F, 'BEGIN{OFS=","}{
+  if ($6 == "QUALITY_COUNTY_SUBDIVISION_CENTROID" || $6 == "QUALITY_COUNTRY_CENTROID" || $6 == "QUALITY_UNKNOWN" || $6 == "QUALITY_COUNTY_SUBDIVISION_CENTROID")  {
+    print
+  }}' addresses-clean.csv | sort -t, -k2 >failures.csv
 
 
