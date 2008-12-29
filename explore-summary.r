4cd4544c8236d021f5f2c7947f766ef8c3c93a80
hadley <h.wickham@gmail.com>
2008-12-29 08:54:40 -0600

    Explore effect of interest rates

--- explore-summary.r
+++ explore-summary.r
@@ -13,15 +13,20 @@ geo$year <- year(geo$date)
 geo <- merge(geo, cpi, by = c("month", "year"), sort = F)
 geo$priceadj <- geo$price * geo$ratio
 
+# Add interest rates
+irates$date <- NULL
+geo <- merge(geo, irates, by = c("month", "year"), sort = F)
 
 # Basic summaries of data --------------------------------------
 daily <- ddply(geo, .(date), function(df) {
   data.frame(
     n = nrow(df),
     avg_priceadj = mean(df$priceadj, na.rm = TRUE), 
-    avg_price = mean(df$price, na.rm = TRUE)
+    avg_price = mean(df$price, na.rm = TRUE),
+    cpi = df$cpi[1],
+    mprime = df$mprime[1]
   )
-})
+}, .progress = "text")
 
 qplot(date, n, data = daily, geom = "line", ylab = "Number of sales") + xlab(NULL)
 ggsave(file = "beautiful-data/graphics/daily-sales.pdf", width = 8, height = 4)
@@ -29,6 +34,9 @@ ggsave(file = "beautiful-data/graphics/daily-sales.pdf", width = 8, height = 4)
 qplot(date, avg_price / 1e6, data = daily, geom = "line", ylab = "Average price (millions)") + xlab(NULL)
 ggsave(file = "beautiful-data/graphics/daily-price.pdf", width = 8, height = 4)
 
+qplot(date, mprime, data = daily, geom = "line")
+qplot(mprime, avg_price, data = daily)
+qplot(mprime, n, data = daily)
 
 ggplot(daily, aes(x = date)) + 
   geom_line(aes(y = avg_price / 1e6), colour = "grey60") + 
--- finances.r
+++ finances.r
@@ -1,4 +1,6 @@
 library(ggplot2)
+source("date.r")
+
 # Inflation ------------------------------------------------------
 
 cpi <- read.csv("cpi-west.csv")
@@ -9,4 +11,25 @@ cpi$ratio <- cpi$cpi[nrow(cpi)] / cpi$cpi
 
 qplot(year + month / 12, cpi / cpi[1], data = cpi, geom = "line", ylab = "Inflation") + xlab(NULL)
 ggsave(file = "beautiful-data/graphics/daily-cpi.pdf", width = 8, height = 4)
+
+
+# Interest rates -------------------------------------------------------------
+# downloaded from http://research.stlouisfed.org/fred2/
+
+
+mprime <- read.csv("finances-mprime.csv", stringsAsFactors = FALSE)
+names(mprime) <- c("date", "mprime")
+mprime$date <- as.Date(mprime$date)
+
+fedfunds <- read.csv("finances-fedfunds.csv", stringsAsFactors = FALSE)
+names(fedfunds) <- c("date", "fedfunds")
+fedfunds$date <- as.Date(fedfunds$date)
+
+irates <- subset(merge(mprime, fedfunds, by = "date"), date > as.Date("2003-03-01"))
+
+qplot(mprime, fedfunds, data = irates)
+with(irates, cor(mprime, fedfunds)) # 0.998 correlation
+
+irates$month <- month(irates$date)
+irates$year <- year(irates$date)
 
